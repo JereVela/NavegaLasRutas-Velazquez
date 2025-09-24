ba952c64e7177911743adc573aa4b4f15205ba49
Jeremias Velazquez <jeremiasmarinsalda@gmail.com>
2025-09-23 22:03:42 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE || '/'
-
 export default defineConfig({
-  base,
+  base: '/NavegaLasRutas-Velazquez/',  
   plugins: [react()],
 })
